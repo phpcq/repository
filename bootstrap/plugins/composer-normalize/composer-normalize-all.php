7c3bff0f82a667371ac26bb025a8e7b0a8e7e00a
David Molineus <david.molineus@netzmacht.de>
2020-05-28 13:21:14 +0200

    Add post processor for composer-normalize

--- bootstrap/plugins/composer-normalize/composer-normalize-all.php
+++ bootstrap/plugins/composer-normalize/composer-normalize-all.php
@@ -3,6 +3,9 @@
 use Phpcq\PluginApi\Version10\BuildConfigInterface;
 use Phpcq\PluginApi\Version10\ConfigurationOptionsBuilderInterface;
 use Phpcq\PluginApi\Version10\ConfigurationPluginInterface;
+use Phpcq\PluginApi\Version10\OutputInterface;
+use Phpcq\PluginApi\Version10\PostProcessorInterface;
+use Phpcq\PluginApi\Version10\ReportInterface;
 
 return new class implements ConfigurationPluginInterface {
     public function getName(): string
@@ -32,12 +35,13 @@ return new class implements ConfigurationPluginInterface {
     {
         yield $buildConfig
             ->getTaskFactory()
-            ->buildRunPhar('composer-normalize', $this->buildArguments($config, $buildConfig))
+            ->buildRunPhar('composer-normalize', $this->buildArguments($config))
             ->withWorkingDirectory($buildConfig->getProjectConfiguration()->getProjectRootPath())
+            ->withPostProcessor($this->createPostProcessor($config['file'] ?? 'composer.json'))
             ->build();
     }
 
-    private function buildArguments(array $config, BuildConfigInterface $buildConfig): array
+    private function buildArguments(array $config): array
     {
         $arguments = [];
 
@@ -64,5 +68,36 @@ return new class implements ConfigurationPluginInterface {
         }
 
         return $arguments;
+    }
+
+    private function createPostProcessor(string $composerFile): PostProcessorInterface
+    {
+        return new class($composerFile) implements PostProcessorInterface {
+            private $composerFile;
+
+            public function __construct(string $composerFile)
+            {
+                $this->composerFile = $composerFile;
+            }
+
+            public function process(
+                ReportInterface $report,
+                array $consoleOutput,
+                int $exitCode,
+                OutputInterface $output
+            ): void {
+                if ($exitCode === 0) {
+                    $severity = 'info';
+                    $status   = ReportInterface::STATUS_PASSED;
+                } else {
+                    $severity = 'error';
+                    $status   = ReportInterface::STATUS_FAILED;
+                }
+
+                $report->addToolReport('composer-normalize', $status);
+                $report->addCheckstyle($this->composerFile)
+                    ->add($severity, implode("\n", $consoleOutput), 'composer-normalize');
+            }
+        };
     }
 };
