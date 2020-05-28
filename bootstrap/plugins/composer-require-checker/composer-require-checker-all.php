ff37f0de040777574efec1c7ac95efce250c9763
David Molineus <david.molineus@netzmacht.de>
2020-05-28 13:27:25 +0200

    Add post processor for composer-require-checker

--- bootstrap/plugins/composer-require-checker/composer-require-checker-all.php
+++ bootstrap/plugins/composer-require-checker/composer-require-checker-all.php
@@ -3,6 +3,9 @@
 use Phpcq\PluginApi\Version10\BuildConfigInterface;
 use Phpcq\PluginApi\Version10\ConfigurationOptionsBuilderInterface;
 use Phpcq\PluginApi\Version10\ConfigurationPluginInterface;
+use Phpcq\PluginApi\Version10\OutputInterface;
+use Phpcq\PluginApi\Version10\PostProcessorInterface;
+use Phpcq\PluginApi\Version10\ReportInterface;
 
 return new class implements ConfigurationPluginInterface {
     public function getName(): string
@@ -29,6 +32,7 @@ return new class implements ConfigurationPluginInterface {
             ->getTaskFactory()
             ->buildRunPhar('composer-require-checker', $this->buildArguments($config, $buildConfig))
             ->withWorkingDirectory($buildConfig->getProjectConfiguration()->getProjectRootPath())
+            ->withPostProcessor($this->createPostProcessor($config['composer_file'] ?? 'composer.json'))
             ->build();
     }
 
@@ -46,5 +50,36 @@ return new class implements ConfigurationPluginInterface {
         }
 
         return $arguments;
+    }
+
+    private function createPostProcessor(string $composerFile): PostProcessorInterface
+    {
+        return new class($composerFile) implements PostProcessorInterface {
+            private $composerFile;
+
+            public function __construct(string $composerFile)
+            {
+                $this->composerFile = $composerFile;
+            }
+
+            public function process(
+                ReportInterface $report,
+                array $consoleOutput,
+                int $exitCode,
+                OutputInterface $output
+            ): void {
+                if ($exitCode === 0) {
+                    $severity = 'info';
+                    $status   = ReportInterface::STATUS_PASSED;
+                } else {
+                    $severity = 'error';
+                    $status   = ReportInterface::STATUS_FAILED;
+                }
+
+                $report->addToolReport('composer-require-checker', $status);
+                $report->addCheckstyle($this->composerFile)
+                    ->add($severity, implode("\n", $consoleOutput), 'composer-require-checker');
+            }
+        };
     }
 };
