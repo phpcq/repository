bedfd8213e67a45f7c54ee3d833fd9e4b44d197c
Christian Schiffler <c.schiffler@cyberspectrum.de>
2020-06-02 18:56:14 +0200

    Rewrite `composer-require-checker-all.php` using output transformer

--- bootstrap/plugins/composer-require-checker/composer-require-checker-all.php
+++ bootstrap/plugins/composer-require-checker/composer-require-checker-all.php
@@ -3,10 +3,10 @@
 use Phpcq\PluginApi\Version10\BuildConfigInterface;
 use Phpcq\PluginApi\Version10\ConfigurationOptionsBuilderInterface;
 use Phpcq\PluginApi\Version10\ConfigurationPluginInterface;
-use Phpcq\PluginApi\Version10\OutputInterface;
-use Phpcq\PluginApi\Version10\PostProcessorInterface;
+use Phpcq\PluginApi\Version10\OutputTransformerInterface;
 use Phpcq\PluginApi\Version10\ReportInterface;
 use Phpcq\PluginApi\Version10\ToolReportInterface;
+use Phpcq\PluginApi\Version10\Util\BufferedLineReader;
 
 return new class implements ConfigurationPluginInterface {
     public function getName(): string
@@ -33,7 +33,7 @@ return new class implements ConfigurationPluginInterface {
             ->getTaskFactory()
             ->buildRunPhar('composer-require-checker', $this->buildArguments($config, $buildConfig))
             ->withWorkingDirectory($buildConfig->getProjectConfiguration()->getProjectRootPath())
-            ->withPostProcessor($this->createPostProcessor($config['composer_file'] ?? 'composer.json'))
+            ->withOutputTransformer($this->createOutputTransformer($config['composer_file'] ?? 'composer.json'))
             ->build();
     }
 
@@ -53,32 +53,156 @@ return new class implements ConfigurationPluginInterface {
         return $arguments;
     }
 
-    private function createPostProcessor(string $composerFile): PostProcessorInterface
+    private function createOutputTransformer(string $composerFile): OutputTransformerInterface
     {
-        return new class($composerFile) implements PostProcessorInterface {
+        return new class($composerFile) implements OutputTransformerInterface {
+            /** @var string */
             private $composerFile;
+            /** @var BufferedLineReader */
+            private $data;
+            /** @var ToolReportInterface */
+            private $report;
 
             public function __construct(string $composerFile)
             {
                 $this->composerFile = $composerFile;
             }
 
-            public function process(
-                ToolReportInterface $report,
-                string $consoleOutput,
-                int $exitCode,
-                OutputInterface $output
-            ): void {
-                if ($exitCode === 0) {
-                    $severity = 'info';
-                    $status   = ReportInterface::STATUS_PASSED;
-                } else {
-                    $severity = 'error';
-                    $status   = ReportInterface::STATUS_FAILED;
-                }
+            public function attach(ToolReportInterface $report): void
+            {
+                $this->report = $report;
+                $this->data   = new BufferedLineReader();
+            }
 
-                $report->addDiagnostic($severity, trim($consoleOutput), $this->composerFile);
-                $report->finish($status);
+            public function write(string $data, int $channel): void
+            {
+                // Can not single channel, as require checker writes every output to STDOUT except for setup errors.
+                $this->data->push($data);
+            }
+
+            public function detach(int $exitCode): void
+            {
+                $this->process();
+                $this->report->finish(0 === $exitCode
+                    ? ReportInterface::STATUS_PASSED
+                    : ReportInterface::STATUS_FAILED);
+
+                $this->report = null;
+            }
+
+            private function process(): void
+            {
+                // FIXME: this should be a state machine to parse incomplete output but for the moment it works.
+                while (null !== $line = $this->data->fetch()) {
+                    // If it is the version header, ignore it.
+                    // eg. ComposerRequireChecker 2.1.0@0c66698d487fcb5c66cf07108e2180c818fb2e72
+                    if (preg_match('#ComposerRequireChecker .*#', $line)) {
+                        continue;
+                    }
+
+                    // If it is the usage suffix, ignore it.
+                    // check [--config-file CONFIG-FILE] [--ignore-parse-errors] [--] [<composer-json>]
+                    if (preg_match(
+                        '#check \[--config-file CONFIG-FILE] \[--ignore-parse-errors] \[--] \[<composer-json>]#',
+                        $line
+                    )) {
+                        continue;
+                    }
+
+                    // Could be notification of missing dependencies.
+                    // In LocateComposerPackageDirectDependenciesSourceFiles.php line 52:
+                    if (preg_match('#In LocateComposerPackageDirectDependenciesSourceFiles.php#', $line)) {
+                        $countEmpty = 0;
+                        $error = '';
+                        // Buffer up until two empty lines.
+                        while (null !== $line = $this->data->fetch()) {
+                            $line = trim($line);
+                            if (empty($line)) {
+                                $countEmpty++;
+                                if (2 === $countEmpty) {
+                                    break;
+                                }
+                                continue;
+                            }
+                            $countEmpty = 0;
+                            $error .= $line;
+                        }
+                        $this->report->addDiagnostic('error', $error, $this->composerFile);
+                    }
+
+
+                    // Missing dependencies found, parse the table.
+                    if (preg_match('#The following unknown symbols were found:#', $line)) {
+                        /*
+                        The following unknown symbols were found:
+                        +----------------+--------------------+
+                        | unknown symbol | guessed dependency |
+                        +----------------+--------------------+
+                        | DOMDocument    | ext-dom            |
+                        | DOMElement     | ext-dom            |
+                        | DOMNode        | ext-dom            |
+                        +----------------+--------------------+
+                        */
+
+                        // Strip table head.
+                        foreach ([
+                            '#^\+-*\+-*\+$#',
+                            '#\|\s*unknown symbol\s*\|\s*guessed dependency\s*\|#',
+                            '#^\+-*\+-*\+$#',
+                        ] as $regex) {
+                            if (1 !== preg_match($regex, $line = $this->data->fetch())) {
+                                throw new \RuntimeException('Failed to parse line: ' . $line);
+                            }
+                        }
+                        // List missing dependencies.
+                        $dependencies = [];
+                        $unknown      = [];
+                        while (null !== $line = $this->data->fetch()) {
+                            if (preg_match('#^\+-*\+-*\+$#', $line)) {
+                                // End of table.
+                                break;
+                            }
+                            if (!preg_match('#\|\s*(?<symbol>.*)\s*\|\s*(?<dependency>.*)?\s*\|#', $line, $matches)) {
+                                throw new \RuntimeException('Failed to parse line: ' . $line);
+                            }
+                            $dependency = trim($matches['dependency'] ?? '');
+                            $symbol     = trim($matches['symbol'] ?? '');
+                            if ('' === $dependency) {
+                                $unknown[] = $symbol;
+                                continue;
+                            }
+                            if (!isset($dependencies[$dependency])) {
+                                $dependencies[$dependency] = [];
+                            }
+                            $dependencies[$dependency][] = $symbol;
+                        }
+                        foreach ($dependencies as $dependency => $symbols) {
+                            $this->report->addDiagnostic(
+                                'error',
+                                sprintf(
+                                    'Missing dependency "%1$s" (used symbols: "%2$s")',
+                                    $dependency,
+                                    implode('", "', $symbols)
+                                ),
+                                $this->composerFile
+                            );
+                        }
+                        if (!empty($unknown)) {
+                            $this->report->addDiagnostic(
+                                'error',
+                                sprintf(
+                                    'Unknown symbols found: "%1$s" - is there a dependency missing?',
+                                    implode('", "', $unknown)
+                                ),
+                                $this->composerFile
+                            );
+                        }
+                    }
+
+                    if (preg_match('#There were no unknown symbols found.#', $line)) {
+                        $this->report->addDiagnostic('info', $line, $this->composerFile);
+                    }
+                }
             }
         };
     }
--- bootstrap/tests/ComposerRequireChecker/ComposerRequireCheckerAllTest.php
+++ bootstrap/tests/ComposerRequireChecker/ComposerRequireCheckerAllTest.php
@@ -7,6 +7,14 @@ namespace Phpcq\BootstrapTest\ComposerRequireChecker;
 use Phpcq\BootstrapTest\BootstrapTestCase;
 use Phpcq\BootstrapTest\Test\BuildConfigBuilder;
 use Phpcq\BootstrapTest\ConfigurationPluginTestCaseTrait;
+use Phpcq\PluginApi\Version10\BuildConfigInterface;
+use Phpcq\PluginApi\Version10\ConfigurationPluginInterface;
+use Phpcq\PluginApi\Version10\OutputInterface;
+use Phpcq\PluginApi\Version10\OutputTransformerInterface;
+use Phpcq\PluginApi\Version10\TaskFactoryInterface;
+use Phpcq\PluginApi\Version10\TaskRunnerBuilderInterface;
+use Phpcq\PluginApi\Version10\TaskRunnerInterface;
+use Phpcq\PluginApi\Version10\ToolReportInterface;
 
 class ComposerRequireCheckerAllTest extends BootstrapTestCase
 {
@@ -84,5 +92,144 @@ class ComposerRequireCheckerAllTest extends BootstrapTestCase
             ->hasProjectRootPath();
 
         $this->assertPluginCreatesMatchingTasksForConfiguration($expected, $configuration);
+    }
+
+    public function outputTransformProvider(): array
+    {
+        return [
+            [
+                'expected' => [
+                    [
+                        'error',
+                        'The composer dependencies have not been installed, run composer install/update first',
+                        'composer.json',
+                    ],
+                ],
+                'input'    => <<<EOF
+                    In LocateComposerPackageDirectDependenciesSourceFiles.php line 52:
+
+                      The composer dependencies have not been installed, run composer install/upd
+                      ate first
+
+
+                    check [--config-file CONFIG-FILE] [--ignore-parse-errors] [--] [<composer-json>]
+
+                EOF
+            ],
+            [
+                'expected' => [
+                    [
+                        'info',
+                        'There were no unknown symbols found.',
+                        'composer.json',
+                    ],
+                ],
+                'input'    => <<<EOF
+                ComposerRequireChecker 2.1.0@0c66698d487fcb5c66cf07108e2180c818fb2e72
+
+                There were no unknown symbols found.
+
+                EOF
+            ],
+            [
+                'expected' => [
+                    [
+                        'error',
+                        'Missing dependency "ext-dom" (used symbols: "DOMDocument", "DOMElement", "DOMNode")',
+                        'composer.json',
+                    ],
+                ],
+                'input'    => <<<EOF
+                ComposerRequireChecker 2.1.0@0c66698d487fcb5c66cf07108e2180c818fb2e72
+                The following unknown symbols were found:
+                +----------------+--------------------+
+                | unknown symbol | guessed dependency |
+                +----------------+--------------------+
+                | DOMDocument    | ext-dom            |
+                | DOMElement     | ext-dom            |
+                | DOMNode        | ext-dom            |
+                +----------------+--------------------+
+
+                EOF
+            ],
+            [
+                'expected' => [
+                    [
+                        'error',
+                        'Missing dependency "ext-dom" (used symbols: "DOMDocument", "DOMElement", "DOMNode")',
+                        'composer.json',
+                    ],
+                    [
+                        'error',
+                        'Unknown symbols found: "Foo\Bar\Baz" - is there a dependency missing?',
+                        'composer.json',
+                    ],
+                ],
+                'input'    => <<<EOF
+                ComposerRequireChecker 2.1.0@0c66698d487fcb5c66cf07108e2180c818fb2e72
+                The following unknown symbols were found:
+                +----------------+--------------------+
+                | unknown symbol | guessed dependency |
+                +----------------+--------------------+
+                | DOMDocument    | ext-dom            |
+                | DOMElement     | ext-dom            |
+                | DOMNode        | ext-dom            |
+                | Foo\Bar\Baz    |                    |
+                +----------------+--------------------+
+
+                EOF
+            ],
+        ];
+    }
+
+    /** @dataProvider outputTransformProvider */
+    public function testTransformsOutput(array $expected, string $input): void
+    {
+        $transformer = $this->mockOutputTransformer([]);
+
+        $report = $this->getMockForAbstractClass(ToolReportInterface::class);
+        $report
+            ->expects($this->exactly(count($expected)))
+            ->method('addDiagnostic')
+            ->withConsecutive(...$expected);
+
+        $transformer->attach($report);
+        $transformer->write($input, OutputInterface::CHANNEL_STDOUT);
+        $transformer->detach(0);
+    }
+
+    public function mockOutputTransformer(array $configurationValues): OutputTransformerInterface
+    {
+        $outputTransformer = null;
+        $builder = $this->getMockForAbstractClass(TaskRunnerBuilderInterface::class);
+        $builder->expects($this->once())->method('withWorkingDirectory')->willReturnSelf();
+        $builder
+            ->expects($this->once())
+            ->method('withOutputTransformer')
+            ->willReturnCallback(function (OutputTransformerInterface $transformer) use (&$outputTransformer, $builder) {
+                $outputTransformer = $transformer;
+
+                return $builder;
+        });
+        $builder->expects($this->once())->method('build')->willReturn($this->getMockForAbstractClass(TaskRunnerInterface::class));
+
+        $taskFactory = $this->getMockForAbstractClass(TaskFactoryInterface::class);
+        $taskFactory->expects($this->once())->method('buildRunPhar')->willReturn($builder);
+
+        $buildConfig = $this->getMockForAbstractClass(BuildConfigInterface::class);
+        $buildConfig->expects($this->once())->method('getTaskFactory')->willReturn($taskFactory);
+
+        /** @var ConfigurationPluginInterface $instance */
+        $instance = static::getPluginInstance();
+        $this->assertSame(basename(dirname(static::getBootstrapFile())), $instance->getName());
+
+        // Have to trigger the lazy generator.
+        foreach ($instance->processConfig($configurationValues, $buildConfig) as $item) {
+            break;
+        }
+
+        $this->assertInstanceOf(OutputTransformerInterface::class, $outputTransformer);
+
+        return $outputTransformer;
     }
 }
