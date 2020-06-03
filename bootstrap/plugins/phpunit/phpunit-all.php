e147e53f8aad15893157ec1295b23bd53c54b4fe
David Molineus <david.molineus@netzmacht.de>
2020-06-03 15:54:00 +0200

    Update usage of appenders
    API of the appenders has changed

--- bootstrap/plugins/phpunit/phpunit-all.php
+++ bootstrap/plugins/phpunit/phpunit-all.php
@@ -3,9 +3,6 @@
 use Phpcq\PluginApi\Version10\BuildConfigInterface;
 use Phpcq\PluginApi\Version10\ConfigurationOptionsBuilderInterface;
 use Phpcq\PluginApi\Version10\ConfigurationPluginInterface;
-use Phpcq\PluginApi\Version10\OutputTransformerFactoryInterface;
-use Phpcq\PluginApi\Version10\OutputTransformerInterface;
-use Phpcq\PluginApi\Version10\ToolReportInterface;
 use Phpcq\PluginApi\Version10\Util\JUnitReportAppender;
 
 return new class implements ConfigurationPluginInterface {
@@ -37,56 +34,7 @@ return new class implements ConfigurationPluginInterface {
             ->getTaskFactory()
             ->buildRunPhar('phpunit', $args)
             ->withWorkingDirectory($projectRoot)
-            ->withOutputTransformer($this->createOutputTransformerFactory($logFile, $projectRoot))
+            ->withOutputTransformer(JUnitReportAppender::transformFile($logFile, $projectRoot))
             ->build();
-    }
-
-    private function createOutputTransformerFactory(
-        string $logFile,
-        string $rootDir
-    ): OutputTransformerFactoryInterface {
-        return new class ($logFile, $rootDir) implements OutputTransformerFactoryInterface {
-            private $logFile;
-            private $rootDir;
-
-            public function __construct(string $logFile, string $rootDir)
-            {
-                $this->logFile = $logFile;
-                $this->rootDir = $rootDir;
-            }
-
-            public function createFor(ToolReportInterface $report): OutputTransformerInterface
-            {
-                return new class ($this->logFile, $this->rootDir, $report) implements OutputTransformerInterface {
-                    /** @var string */
-                    private $logFile;
-                    /** @var string */
-                    private $rootDir;
-                    /** @var ToolReportInterface */
-                    private $report;
-
-                    public function __construct(string $logFile, string $rootDir, ToolReportInterface $report)
-                    {
-                        $this->logFile = $logFile;
-                        $this->rootDir = $rootDir;
-                        $this->report  = $report;
-                    }
-
-                    public function write(string $data, int $channel): void
-                    {
-                        // FIXME: do we also want to parse stdout/stderr?
-                    }
-
-                    public function finish(int $exitCode): void
-                    {
-                        $this->report->addAttachment($this->logFile, 'junit-log.xml');
-                        JUnitReportAppender::appendTo($this->report, $this->logFile, $this->rootDir);
-                        $this->report->finish(
-                            $exitCode === 0 ? ToolReportInterface::STATUS_PASSED : ToolReportInterface::STATUS_FAILED
-                        );
-                    }
-                };
-            }
-        };
     }
 };
