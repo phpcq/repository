f72650d30cbc31b919fd99252c10d5a7261cdcfd
David Molineus <david.molineus@netzmacht.de>
2020-06-12 17:57:58 +0200

    Improve translations
    Import dictionary from https://github.com/sebastianbergmann/phploc/blob/master/src/Log/Csv.php use auto translation as fallback for untranslated keys

--- bootstrap/plugins/phploc/phploc-all.php
+++ bootstrap/plugins/phploc/phploc-all.php
@@ -98,12 +98,60 @@ return new class implements ConfigurationPluginInterface {
             {
                 return new class ($this->xmlFile, $report) implements OutputTransformerInterface {
                     private const DICTIONARY = [
-                        'ccn'   => 'Cyclomatic Complexity',
-                        'lloc'  => 'Logical Lines of Code',
-                        'ncloc' => 'Non-Comment Lines of Code (NCLOC)',
-                        'min'   => 'Minimum',
-                        'max'   => 'Maximum',
-                        'avg'   => 'Average',
+                        // LIST copied from https://github.com/sebastianbergmann/phploc/blob/master/src/Log/Csv.php
+                        'directories'                 => 'Directories',
+                        'files'                       => 'Files',
+                        'loc'                         => 'Lines of Code (LOC)',
+                        'ccnByLloc'                   => 'Cyclomatic Complexity / Lines of Code',
+                        'cloc'                        => 'Comment Lines of Code (CLOC)',
+                        'ncloc'                       => 'Non-Comment Lines of Code (NCLOC)',
+                        'lloc'                        => 'Logical Lines of Code (LLOC)',
+                        'llocGlobal'                  => 'LLOC outside functions or classes',
+                        'namespaces'                  => 'Namespaces',
+                        'interfaces'                  => 'Interfaces',
+                        'traits'                      => 'Traits',
+                        'classes'                     => 'Classes',
+                        'abstractClasses'             => 'Abstract Classes',
+                        'concreteClasses'             => 'Concrete Classes',
+                        'finalClasses'                => 'Final Classes',
+                        'nonFinalClasses'             => 'Non-Final Classes',
+                        'llocClasses'                 => 'Classes Length (LLOC)',
+                        'methods'                     => 'Methods',
+                        'nonStaticMethods'            => 'Non-Static Methods',
+                        'staticMethods'               => 'Static Methods',
+                        'publicMethods'               => 'Public Methods',
+                        'nonPublicMethods'            => 'Non-Public Methods',
+                        'protectedMethods'            => 'Protected Methods',
+                        'privateMethods'              => 'Private Methods',
+                        'classCcnAvg'                 => 'Cyclomatic Complexity / Number of Classes',
+                        'methodCcnAvg'                => 'Cyclomatic Complexity / Number of Methods',
+                        'functions'                   => 'Functions',
+                        'namedFunctions'              => 'Named Functions',
+                        'anonymousFunctions'          => 'Anonymous Functions',
+                        'llocFunctions'               => 'Functions Length (LLOC)',
+                        'llocByNof'                   => 'Average Function Length (LLOC)',
+                        'classLlocAvg'                => 'Average Class Length',
+                        'methodLlocAvg'               => 'Average Method Length',
+                        'averageMethodsPerClass'      => 'Average Methods per Class',
+                        'constants'                   => 'Constants',
+                        'globalConstants'             => 'Global Constants',
+                        'classConstants'              => 'Class Constants',
+                        'publicClassConstants'        => 'Public Class Constants',
+                        'nonPublicClassConstants'     => 'Non-Public Class Constants',
+                        'attributeAccesses'           => 'Attribute Accesses',
+                        'instanceAttributeAccesses'   => 'Non-Static Attribute Accesses',
+                        'staticAttributeAccesses'     => 'Static Attribute Accesses',
+                        'methodCalls'                 => 'Method Calls',
+                        'instanceMethodCalls'         => 'Non-Static Method Calls',
+                        'staticMethodCalls'           => 'Static Method Calls',
+                        'globalAccesses'              => 'Global Accesses',
+                        'globalVariableAccesses'      => 'Global Variable Accesses',
+                        'superGlobalVariableAccesses' => 'Super-Global Variable Accesses',
+                        'globalConstantAccesses'      => 'Global Constant Accesses',
+                        'testClasses'                 => 'Test Classes',
+                        'testMethods'                 => 'Test Methods',
+                        // Custom added words
+                        'ccn'                         => 'Cyclomatic Complexity',
                     ];
 
                     /** @var string */
@@ -170,7 +218,7 @@ return new class implements ConfigurationPluginInterface {
                                 ' ',
                                 array_map(
                                     function (string $word): string {
-                                        return $this->translateWord($word) ?: $word;
+                                        return $this->translateWord(strtolower($word)) ?: $word;
                                     },
                                     preg_split('/(?=[A-Z])/', $text)
                                 )
@@ -180,7 +228,7 @@ return new class implements ConfigurationPluginInterface {
 
                     private function translateWord(string $text): ?string
                     {
-                        return self::DICTIONARY[strtolower($text)] ?? null;
+                        return self::DICTIONARY[$text] ?? null;
                     }
                 };
             }
